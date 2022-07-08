6d6ce65f205c5a5053638037129e88f3ba01b3da
Kiara Kim <101039161+kiarakim@users.noreply.github.com>
2022-07-08 22:16:52 +0900

    add LEV17 cpp file

---
+++ LEV17/hw06.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+// 합친 mask 배열의 좌표 구하기
+
+int main() {
+
+	int a[4][4] = { 0,0,0,1,1,1,0,1,1,0,0,1,1,1,1,1 };
+	int b[4][4] = { 1,1,1,1,1,0,1,1,1,0,0,0,1,0,0,0 };
+
+	for (int i = 0; i < 4; i++) {
+		for (int j = 0; j < 4; j++) {
+			if (!a[i][j] && !b[i][j])
+				cout << "(" << i << "," << j << ")" << endl;
+		}
+	}
+	return 0;
+}
